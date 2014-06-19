0d0ded12ba1726fd5fb18b2ade594c0efc771be6
khanzanicdecay <jake@cereusdesigns.com>
2014-06-19 01:31:09 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -71,9 +71,10 @@ class GSMAParser {
             $st = explode('<h1>', $el,2);
             $tmp = explode('</h1>',$st[1],2);
             $m['name'] = str_replace(" ", "<br>", $tmp[0]);
-            $imgtmp = explode('" src="',$el,2);
-            $imgtmp2 = explode('"',$imgtmp[1]);
-            $im = file_get_contents($imgtmp2[0]);
+            $imgtmp = explode('<div id="specs-cp-pic">',$el);
+            $imgtmp2 = explode('src="',$imgtmp[1]);
+            $imgtmp3 = explode('"',$imgtmp[1]);
+            $im = file_get_contents($imgtmp3[0]);
             $m['img'] = base64_encode($im);
             $tmp = explode(' ', $tmp[0], 2);
             $m['rname'] = $tmp[1];
